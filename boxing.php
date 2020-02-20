42d901597230affa557c9d59a82ea94fe7aca6fd
SoullesA4 <gerardobasilio3g@gmail.com>
2020-02-19 19:01:16 -0600

    Beta15

---
+++ boxing.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Artes Marciales Mixtas Soulless</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body >
+	<header>
+		<h1>ARTES MARCIALES MIXTAS: Dr Soulless</h1>
+	</header>
+	<div  class="boxing">
+		<nav id="menu">
+			<ul>
+				<a href="index.html"> INICIO </a>
+				<a href="mma.html"> MMA </a>
+				<a href="box.html"> BOX </a>
+				<a href="boxing.html"> KICK BOXING </a>
+				<a href="muay.html"> MUAY THAI </a>
+				<a href="jiu.html"> JIU JITSU </a>
+			</ul>
+		</nav>
+		<section id="articulos"  >
+			<article><h2>¿Qué es el Kick Boxing</h2>
+				<p>El kick boxing (en japonés: キックボクシング) es un deporte de combate de origen japonés y desarrollo occidental, en el cual, se mezclan las técnicas del boxeo con las técnicas de patadas de algunas artes marciales como el taekwondo y el muay thai, estando así relacionado de las artes marciales de oriente. Si bien, al kickboxing no se lo considera un arte marcial formativo tradicional o gendai budō donde se busca por excelencia, existen diversas opiniones sobre si se trata de un arte marcial o no.</p>
+			</article>
+			<img alt="Peleador de Kick Boxing" src="img/dee3.png" class="dee">
+			<img alt="Peleador de Kick Boxing" src="img/dee2.png" class="dee1">
+			<article>
+				<h2>¿Qué es UFC y cuál es la diferencia con MMA?</h2>
+				<p> Se considera que un luchador de kick boxing es una competencia muy reñida para los demás peleadores de otros estilos de lucha en pie, que practican otros tipos de deportes de combate o artes marciales, por la gran resistencia física, contundencia y aguante a los golpes de sus practicantes. Actualmente, debido a su practicidad y contundencia es uno de los sistemas preferidos y de mayor divulgación en el desarrollo de la lucha en pie, siendo el preferido por los practicantes de las artes marciales mixtas o MMA/AMM</p>
+
+				<p>El kick boxing tiene sus orígenes en boxeo occidental, el karate japonés, y en el muay thai de Tailandia, también llamado "thai boxing".</p>
+			</article>
+		</section>
+		<aside>
+			<h2>HOLA MUNDO</h2>
+			<p><h3>Batman quiere concocerte!!</h3></p>
+			<p>BATMAN quiere divertirse un poco</p>
+			<p>Si quieres conocerlo ve al callejon más oscuro de tu zona y espera ahi de 10pm a 6am. Descuida, el sabe donde vives</p>
+			<img class="batman" src="img/murcielago_negro.png">
+		</aside>
+		<footer class="fotboxing">
+			<p class="pboxing">
+			Derechos reservados a Soulless.com.mx.xvideos <br>
+			Facebook: Soulles Sauto <br>
+			Gmail: soullessauto@gmail.com <br>
+			GameTag: SoullesA4 <br>
+			Tinder: Pussy Destroyer	<br>
+			</p>
+		</footer>
+	</div>
+</body>
+</html>
---
+++ mma.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Artes Marciales Mixtas Soulless</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body >
+	<header>
+		<h1>ARTES MARCIALES MIXTAS: Dr Soulless</h1>
+	</header>
+	<div  class="mma">
+		<nav id="menu">
+			<ul>
+				<a href="index.html"> INICIO </a>
+				<a href="mma.html"> MMA </a>
+				<a href="box.html"> BOX </a>
+				<a href="boxing.html"> KICK BOXING </a>
+				<a href="muay.html"> MUAY THAI </a>
+				<a href="jiu.html"> JIU JITSU </a>
+			</ul>
+		</nav>
+		<section id="articulos"  >
+			<article><h2>MMA</h2>
+				<p>MMA es una sigla que tiene diversos usos. Uno de los más habituales aparece en el terreno de los deportes de combate y se refiere a las mixed martial arts, expresión inglesa que alude a las artes marciales mixtas</p>
+			</article>
+			<img alt="Peleador de MMA" src="img/guile.png" class="guile">
+			<article>
+				<h2>¿Qué es UFC y cuál es la diferencia con MMA?</h2>
+				<p>En las MMA, por lo tanto, se combinan técnicas de diferentes especialidades para el desarrollo de una disciplina deportiva o de defensa personal. A nivel profesional, las MMA están consideradas como el deporte de mayor contacto ya que permiten golpes con las manos, los codos, los pies y las rodillas, además de avalar los agarres y los derribes.</p>
+				<p>Las MMA, en definitiva, incluyen movimientos y acciones de boxeo, karate, taekwondo, kickboxing, judo y lucha libre, por ejemplo. Hay quienes afirman que sus orígenes se remontan a los Juegos Olímpicos de la Antigua Grecia, aunque la versión moderna se puede rastrear en competencias que, durante el siglo XX, comenzaron a llevarse a cabo en Brasil, Estados Unidos, Japón y varios países europeos.</p>
+			</article>
+		</section>
+		<aside>
+			<h2>HOLA MUNDO</h2>
+			<p><h3>Batman quiere concocerte!!</h3></p>
+			<p>BATMAN quiere divertirse un poco</p>
+			<p>Si quieres conocerlo ve al callejon más oscuro de tu zona y espera ahi de 10pm a 6am. Descuida, el sabe donde vives</p>
+			<img class="batman" src="img/murcielago_negro.png">
+		</aside>
+		<footer class="fotmma">
+			<p class="pmma">
+			Derechos reservados a Soulless.com.mx.xvideos <br>
+			Facebook: Soulles Sauto <br>
+			Gmail: soullessauto@gmail.com <br>
+			GameTag: SoullesA4 <br>
+			Tinder: Pussy Destroyer	<br>	
+			</p>
+		</footer>
+	</div>
+</body>
+</html>
